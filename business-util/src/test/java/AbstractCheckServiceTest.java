18be1440987b355144eb3a9de868144d54ce04fb
593436 <Xuegaoyu3>
2022-10-20 14:45:23 +0800

    test

---
+++ business-util/src/test/java/AbstractCheckServiceTest.java
@@ -0,0 +1,11 @@
+import com.xuegao.AbstractCheckService;
+
+/**
+ * @author fjm
+ * @date 2022年10月20日 14:42
+ */
+public class AbstractCheckServiceTest {
+    public static void main(String[] args) {
+
+    }
+}
